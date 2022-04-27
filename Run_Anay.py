ce5ed712719f42cd9de9647bde88102c11bcc06d
Anatoliy <vittagan@mail.ru>
2022-04-27 18:16:51 +0300

    Новая ветка про Аню

--- Run_Anay.py
+++ Run_Anay.py
@@ -4,5 +4,5 @@ import random
 pygame.init()
 
 def display_game(width,height,title='Аня убегай!!!'):
-    """Функция размера дисплея и заголовка"""
+    """Функция размера дисплея и заголовкq"""
     pygame.display.set_caption(title,'Hello')
